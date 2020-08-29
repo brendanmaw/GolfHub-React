c6abb4b4c440013168c0ca474213aa9a5e863066
Brendan Maw <brendanmaw19@gmail.com>
2020-08-29 15:26:08 -0600

    styling

--- src/Widgets.js
+++ src/Widgets.js
@@ -4,21 +4,8 @@ import "./Widgets.css";
 function Widgets() {
     return (
         <div className="widgets">
-            <iframe 
-                src="https://www.tourbrassie.com/widget/index?
-                type=index&showlogo=0&showfacebook=0&
-                bgcolor=&width=340&height=1500&widthsetting=custom&
-                unit=px&layout=table&country=&displayplayersby=country&
-                playerlist=&showcountry=1&showaveragepoints=0&
-                showtotalpoints=1&showsponsor=1&tour=pga-tour&limit=10"
-                width="340"
-                height="100%"
-                style={{ border: "none", overflow: "hidden" }}
-                scrolling="no"
-                frameborder="0"
-                allowTransparency="false"
-                allow="encrypted-media"
-            ></iframe>
+            <iframe width="300" height="350" src="http://www.owgr.com/Global/Widgets/Left/SharingRankingTableWidget.aspx?country=&continent="frameborder="0" allowfullscreen="false"></iframe>
+           
         </div>
     )
 }
